62bd4c0d08dd490c787cb0c64f2219bb5145a2ff
Paisit Jarunnamsiri <paisitj@Paisits-MacBook-Pro-14.local>
2024-05-21 15:05:48 -0700

    ch05

--- book-swap/README.md
+++ book-swap/README.md
@@ -1,5 +1,11 @@
 # BookSwap
 
+## Run Local
 ```bash
 $ BOOKSWAP_PORT=8080 go run cmd/main.go
+```
+
+## Ginkgo
+```bash
+$ ginkgo -v ./handlers
 ```
---
+++ book-swap/handlers/handlers_test.go
@@ -0,0 +1,47 @@
+package handlers_test
+
+import (
+	"book-swap/db"
+	"book-swap/handlers"
+	"encoding/json"
+	"io"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestIndexIntegration(t *testing.T) {
+	if testing.Short() {
+		t.Skip("Skipping TestIndexIntegration in short mode.")
+	}
+
+	// Arrange
+	book := db.Book{
+		ID:     uuid.New().String(),
+		Name:   "My first intergration test",
+		Status: db.Available.String(),
+	}
+	bs := db.NewBookService([]db.Book{book}, nil)
+	h := handlers.NewHandler(bs, nil)
+	svr := httptest.NewServer(http.HandlerFunc(h.Index))
+	defer svr.Close()
+
+	// Act
+	r, err := http.Get(svr.URL)
+
+	// Assert
+	require.Nil(t, err)
+	assert.Equal(t, http.StatusOK, r.StatusCode)
+	body, err := io.ReadAll(r.Body)
+	r.Body.Close()
+	require.Nil(t, err)
+	var resp handlers.Response
+	err = json.Unmarshal(body, &resp)
+	require.Nil(t, err)
+	assert.Equal(t, 1, len(resp.Books))
+	assert.Contains(t, resp.Books, book)
+}
